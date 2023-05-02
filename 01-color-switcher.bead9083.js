46824d2017b78a4b94d23c879c12d743dff28235
lisovenko02 <118771244+lisovenko02@users.noreply.github.com>
2023-05-02 22:26:40 +0000

    Deploying to gh-pages from @ lisovenko02/goit-js-hw-09@b0d15420256f054dfb08221813c55953cd04c1b3 🚀

---
+++ 01-color-switcher.bead9083.js
@@ -0,0 +1,2 @@
+!function(){var e,t=document.querySelector("body"),d=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");a.disabled=!0,d.addEventListener("click",(function(){d.disabled=!0,a.disabled=!1,e=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(){a.disabled=!0,d.disabled=!1,clearInterval(e)})),console.dir(a)}();
+//# sourceMappingURL=01-color-switcher.bead9083.js.map
